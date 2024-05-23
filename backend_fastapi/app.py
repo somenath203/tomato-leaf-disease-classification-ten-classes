61a738236887c466c363a468186debe38db76ef3
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-05-23 18:58:51 +0530

    commited

--- backend_fastapi/app.py
+++ backend_fastapi/app.py
@@ -81,20 +81,6 @@ model_ensemble_learning = load_model(os.path.join(
     extract_dir_ensemble_learning_model, 'ensemble_learning_model'))
 
 
-app = FastAPI()
-
-
-origins = ["*"]
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 class_names = ['Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot',
                'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
