46dabe721dbbc785df88d1654b0a40ff4763d8ac
Somenath Choudhury <81456073+somenath203@users.noreply.github.com>
2026-06-10 19:38:38 +0530

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,47 +2,36 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const ProtectedRoutes = ({ children }) => {
 
     const navigate = useNavigate();
 
-
-    const getAuhenticatedUserData = async () => {
-
-        try {
-
-            await axios.post(`${process.env.REACT_APP_BACKEND_API_NODEJS}/get-user-profile`, {}, {
-
-                headers: {
-
-                    Authorization: `Bearer ${localStorage.getItem('leaf-proj-token')}`
-                }
-
-            });
-
-
-        } catch (error) {
-
-            localStorage.removeItem('leaf-proj-token');
-
-            navigate('/');
-
-        }
-
-    };
-
     useEffect(() => {
+        const getAuhenticatedUserData = async () => {
+            try {
+                await axios.post(
+                    `${process.env.REACT_APP_BACKEND_API_NODEJS}/get-user-profile`,
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('leaf-proj-token')}`
+                        }
+                    }
+                );
+            } catch (error) {
+                localStorage.removeItem('leaf-proj-token');
+                navigate('/');
+            }
+        };
+
         getAuhenticatedUserData();
-    }, []);
+    }, [navigate]);
 
     return (
         <>
             {children}
         </>
-    )
-
+    );
 };
-
 
 export default ProtectedRoutes;
